6dfb91634ef9f7d193d570c48f18ed778c350baf
Minh-Leo <tranduyminh.bui@gmail.com>
2021-06-18 19:38:04 +1000

    Added tests

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,22 @@
-import assert from "assert";
+// import assert from "assert";
+import "/imports/api/TasksMethods.tests.js";
+import "/imports/api/Tasks.tests.js";
 
-describe("simple-todos-react", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "simple-todos-react");
-  });
+// describe("simple-todos-react", function () {
+//   it("package.json has correct name", async function () {
+//     const { name } = await import("../package.json");
+//     assert.strictEqual(name, "simple-todos-react");
+//   });
 
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
+//   if (Meteor.isClient) {
+//     it("client is not server", function () {
+//       assert.strictEqual(Meteor.isServer, false);
+//     });
+//   }
 
-  if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
-  }
-});
+//   if (Meteor.isServer) {
+//     it("server is not client", function () {
+//       assert.strictEqual(Meteor.isClient, false);
+//     });
+//   }
+// });
